c94d2bb5d4c49b8b26d3c40fe817cae180dd2808
Evan Ananda Putra <121525443+evanputra1@users.noreply.github.com>
2023-11-02 16:52:47 +0700

    membuat kalender

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject, Selection, Toolbar } from '@syncfusion/ej2-react-grids/src'
+import { customersData, customersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        dataSource={customersData}
+        width="auto"
+        allowPaging
+        allowSorting
+        pageSettings={{ pageCount: 5 }}
+        toolbar={['Delete']}
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {customersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[ Page, Toolbar, Selection, Edit, Sort, Filter]} />
+
+      </GridComponent>
+    </div>
   )
 }
 
